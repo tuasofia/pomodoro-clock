b79d7f1d3c5ad27ab391000782c28062b372101f
Veronica Tua <perez_155035@students.pupr.edu>
2025-07-28 20:58:30 -0400

    Added session component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import React from 'react';
-import Break from './components/Break'
+import Break from './components/Break';
 import './App.css';
+import Session from './components/Session';
 
 function App() {
   return (
     <div className="App">
-      <Break />
+          <Break />
+          <Session/>
     </div>
   );
 }
